f67924b0f42ed4fbb332966218f13045af93df3d
Longxing Liu <liulongxingfor@163.com>
2023-05-19 16:41:31 +0800

    xixi

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -47,7 +47,7 @@ var config = {
         "和想我！": "./imgs/nothing.jpg",
         "生日快乐~~": "./imgs/nothing.jpg"
     },
-
+    
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "点我开始",
